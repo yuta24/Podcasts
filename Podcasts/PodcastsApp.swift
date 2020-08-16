a42df5cd5d68f82a4ac35ab5fb8f4c17cafef91a
yuta24 <yuta24@bivre.com>
2020-08-16 22:20:54 +0900

    Initalize project

---
+++ Podcasts/PodcastsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PodcastsApp.swift
+//  Podcasts
+//
+//  Created by Yu Tawata on 2020/08/16.
+//
+
+import SwiftUI
+
+@main
+struct PodcastsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
