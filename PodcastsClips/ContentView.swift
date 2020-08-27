a1ba959a3216cb3b5eb0d39f9f7fc94c00fc51c1
yuta24 <yuta24@bivre.com>
2020-08-27 22:10:44 +0900

    Add AppClips

---
+++ PodcastsClips/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PodcastsClips
+//
+//  Created by Yu Tawata on 2020/08/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PodcastsClips/PodcastsClipsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PodcastsClipsApp.swift
+//  PodcastsClips
+//
+//  Created by Yu Tawata on 2020/08/27.
+//
+
+import SwiftUI
+
+@main
+struct PodcastsClipsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
