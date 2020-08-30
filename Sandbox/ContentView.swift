155b1dc8710f8cf86943abb632c80889415557cf
yuta24 <yuta24@bivre.com>
2020-08-30 12:45:37 +0900

    Add UI framework

---
+++ Sandbox/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Sandbox
+//
+//  Created by Yu Tawata on 2020/08/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Sandbox/SandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  SandboxApp.swift
+//  Sandbox
+//
+//  Created by Yu Tawata on 2020/08/30.
+//
+
+import SwiftUI
+
+@main
+struct SandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
